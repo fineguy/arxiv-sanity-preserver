016717f527b2bd3b1c6b19fa16ae9de47be18fc6
Timofei Semenov <timasem@gmail.com>
2017-07-17 13:20:02 +0200

    use multithreading for faster downloading

--- download_pdfs.py
+++ download_pdfs.py
@@ -1,44 +1,56 @@
+from multiprocessing.dummy import Pool
 import os
-import time
 import pickle
-import shutil
-import random
-from  urllib.request import urlopen
+import time
+from urllib.request import urlretrieve
 
 from utils import Config
 
-timeout_secs = 10 # after this many seconds we give up on a paper
-if not os.path.exists(Config.pdf_dir): os.makedirs(Config.pdf_dir)
-have = set(os.listdir(Config.pdf_dir)) # get list of all pdfs we already have
+RETRIES = 5  # maximum number of retries for fetching a pdf
+TIMEOUT = 10  # sleep time in case of a fetching failure
+THREADS = 40  # number of downloading threads
+if not os.path.exists(Config.pdf_dir):
+  os.makedirs(Config.pdf_dir)
+EXISTING_PAPERS = set(os.listdir(Config.pdf_dir))  # get list of all pdfs we already have
+NUM_OK = 0
+DB = pickle.load(open(Config.db_path, 'rb'))
 
-numok = 0
-numtot = 0
-db = pickle.load(open(Config.db_path, 'rb'))
-for pid,j in db.items():
-  
+
+def save_pdf(pid, j):
   pdfs = [x['href'] for x in j['links'] if x['type'] == 'application/pdf']
   assert len(pdfs) == 1
   pdf_url = pdfs[0] + '.pdf'
-  basename = pdf_url.split('/')[-1]
+  basename = os.path.basename(pdf_url)
   fname = os.path.join(Config.pdf_dir, basename)
 
-  # try retrieve the pdf
-  numtot += 1
+  def download(retries=RETRIES, timeout=TIMEOUT):
+    success = False
+    for i in range(RETRIES):
+      try:
+        urlretrieve(pdf_url, fname)
+        success = True
+        break
+      except Exception:
+        time.sleep(TIMEOUT)
+    return success
+
+  global NUM_OK
   try:
-    if not basename in have:
-      print('fetching %s into %s' % (pdf_url, fname))
-      req = urlopen(pdf_url, None, timeout_secs)
-      with open(fname, 'wb') as fp:
-          shutil.copyfileobj(req, fp)
-      time.sleep(0.05 + random.uniform(0,0.1))
+    if basename not in EXISTING_PAPERS:
+      success = download()
+      if success:
+        print('fetched %s into %s' % (pdf_url, fname))
+      else:
+        print('error downloading: ', pdf_url)
     else:
       print('%s exists, skipping' % (fname, ))
-    numok+=1
+    NUM_OK += 1
   except Exception as e:
     print('error downloading: ', pdf_url)
     print(e)
-  
-  print('%d/%d of %d downloaded ok.' % (numok, numtot, len(db)))
-  
-print('final number of papers downloaded okay: %d/%d' % (numok, len(db)))
 
+
+with Pool(THREADS) as pool:
+  pool.starmap(save_pdf, DB.items())
+
+print('final number of papers downloaded okay: %d/%d' % (NUM_OK, len(DB)))
