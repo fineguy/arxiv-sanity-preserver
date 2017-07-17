faacd499253a610b81d316a8235ffeb9a1d70adb
Timofei Semenov <timasem@gmail.com>
2017-07-17 15:48:11 +0200

    add retries for empty responses from arxiv

--- fetch_papers.py
+++ fetch_papers.py
@@ -4,7 +4,6 @@ The script is intended to enrich an existing database pickle (by default db.p),
 so this file will be loaded first, and then new results will be added to it.
 """
 
-import os
 import time
 import pickle
 import random
@@ -14,9 +13,15 @@ import feedparser
 
 from utils import Config, safe_pickle_dump
 
+RETRIES = 5  # maximum number of retries for sending a query
+TIMEOUT = 600  # sleep time in case of a query failure
+BASE_URL = 'http://export.arxiv.org/api/query?search_query=%s&' \
+           'sortBy=lastUpdatedDate&start=%i&max_results=%i'  # base api query url
+
+
 def encode_feedparser_dict(d):
-  """ 
-  helper function to get rid of feedparser bs with a deep copy. 
+  """
+  Helper function to get rid of feedparser bs with a deep copy.
   I hate when libs wrap simple things in their own classes.
   """
   if isinstance(d, feedparser.FeedParserDict) or isinstance(d, dict):
@@ -32,33 +37,54 @@ def encode_feedparser_dict(d):
   else:
     return d
 
+
 def parse_arxiv_url(url):
-  """ 
-  examples is http://arxiv.org/abs/1512.08756v2
-  we want to extract the raw id and the version
   """
-  ix = url.rfind('/')
-  idversion = url[ix+1:] # extract just the id (and the version)
+  Examples is http://arxiv.org/abs/1512.08756v2
+  We want to extract the raw id and the version
+  """
+  idversion = url.rsplit('/', 1)[-1]  # extract just the id (and the version)
   parts = idversion.split('v')
   assert len(parts) == 2, 'error parsing url ' + url
   return parts[0], int(parts[1])
 
-if __name__ == "__main__":
 
-  # parse input arguments
+def send_request(query, index, rpi, retries=RETRIES, timeout=TIMEOUT):
+  """
+  Attempt new requests in case arxiv returns an empty list.
+  """
+  url_query = BASE_URL % (query, index, rpi)
+  for i in range(1, retries + 1):
+    with urllib.request.urlopen(url_query) as url:
+      response = url.read()
+    parse = feedparser.parse(response)
+    if len(parse.entries) == 0 and i < retries:
+      print("Request %d out of %d failed, retry in %.2f seconds" % (i, retries, timeout))
+      time.sleep(timeout)
+      continue
+    return response, parse.entries
+
+
+def parse_args():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--search-query', type=str,
-                      default='cat:cs.CV+OR+cat:cs.AI+OR+cat:cs.LG+OR+cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML',
-                      help='query used for arxiv API. See http://arxiv.org/help/api/user-manual#detailed_examples')
+  parser.add_argument('--search-query', type=str, default='cat:cs.CV+OR+cat:cs.AI+OR+cat:cs.LG+OR+'
+                      'cat:cs.CL+OR+cat:cs.NE+OR+cat:stat.ML', help='query used for arxiv API. '
+                      'See http://arxiv.org/help/api/user-manual#detailed_examples')
   parser.add_argument('--start-index', type=int, default=0, help='0 = most recent API result')
-  parser.add_argument('--max-index', type=int, default=10000, help='upper bound on paper index we will fetch')
+  parser.add_argument('--max-index', type=int, default=10000,
+                      help='upper bound on paper index we will fetch')
   parser.add_argument('--results-per-iteration', type=int, default=100, help='passed to arxiv API')
-  parser.add_argument('--wait-time', type=float, default=5.0, help='lets be gentle to arxiv API (in number of seconds)')
-  parser.add_argument('--break-on-no-added', type=int, default=1, help='break out early if all returned query papers are already in db? 1=yes, 0=no')
-  args = parser.parse_args()
+  parser.add_argument('--wait-time', type=float, default=5.0,
+                      help='lets be gentle to arxiv API (in number of seconds)')
+  parser.add_argument('--no-break-on-no-added', action='store_false', help='no break out early '
+                      'if all returned query papers are already in db')
+  return parser.parse_args()
+
+
+if __name__ == "__main__":
+  args = parse_args()
 
   # misc hardcoded variables
-  base_url = 'http://export.arxiv.org/api/query?' # base api query url
   print('Searching arXiv for %s' % (args.search_query, ))
 
   # lets load the existing database to memory
@@ -74,18 +100,14 @@ if __name__ == "__main__":
   # main loop where we fetch the new results
   print('database has %d entries at start' % (len(db), ))
   num_added_total = 0
-  for i in range(args.start_index, args.max_index, args.results_per_iteration):
-
-    print("Results %i - %i" % (i,i+args.results_per_iteration))
-    query = 'search_query=%s&sortBy=lastUpdatedDate&start=%i&max_results=%i' % (args.search_query,
-                                                         i, args.results_per_iteration)
-    with urllib.request.urlopen(base_url+query) as url:
-      response = url.read()
-    parse = feedparser.parse(response)
+  start_index = args.start_index
+  for index in range(args.start_index, args.max_index, args.results_per_iteration):
+    print("Results %i - %i" % (index, index + args.results_per_iteration))
+    response, entries = send_request(args.search_query, index, args.results_per_iteration)
     num_added = 0
     num_skipped = 0
-    for e in parse.entries:
 
+    for e in entries:
       j = encode_feedparser_dict(e)
 
       # extract just the raw arxiv id and version for this paper
@@ -94,31 +116,31 @@ if __name__ == "__main__":
       j['_version'] = version
 
       # add to our database if we didn't have it before, or if this is a new version
-      if not rawid in db or j['_version'] > db[rawid]['_version']:
+      if rawid not in db or j['_version'] > db[rawid]['_version']:
         db[rawid] = j
         print('Updated %s added %s' % (j['updated'].encode('utf-8'), j['title'].encode('utf-8')))
         num_added += 1
-        num_added_total += 1
       else:
         num_skipped += 1
+
+    num_added_total += num_added
 
     # print some information
     print('Added %d papers, already had %d.' % (num_added, num_skipped))
 
-    if len(parse.entries) == 0:
+    if len(entries) == 0:
       print('Received no results from arxiv. Rate limiting? Exiting. Restart later maybe.')
       print(response)
       break
 
-    if num_added == 0 and args.break_on_no_added == 1:
+    if num_added == 0 and args.no_break_on_no_added:
       print('No new papers were added. Assuming no new papers exist. Exiting.')
       break
 
-    print('Sleeping for %i seconds' % (args.wait_time , ))
+    print('Sleeping for %.2f seconds' % (args.wait_time, ))
     time.sleep(args.wait_time + random.uniform(0, 3))
 
   # save the database before we quit, if we found anything new
   if num_added_total > 0:
     print('Saving database with %d papers to %s' % (len(db), Config.db_path))
     safe_pickle_dump(db, Config.db_path)
-
